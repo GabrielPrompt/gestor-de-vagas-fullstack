a7cee1a2859aa133580b8ac3531465f37816eb55
Gabriel Bueno De Camargo <135550114+GabrielPrompt@users.noreply.github.com>
2025-12-17 02:32:28 -0300

    Alterando configurações do CORS

--- backend/src/main/java/project/gestor_de_vagas_api/config/CorsConfig.java
+++ backend/src/main/java/project/gestor_de_vagas_api/config/CorsConfig.java
@@ -17,8 +17,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowedOrigins(List.of(
-                "http://127.0.0.1:5500",
-                "http://localhost:5500"
+                "http://localhost:3000"
         ));
 
         config.setAllowedMethods(List.of(
